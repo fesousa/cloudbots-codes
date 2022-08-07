65660a3783caf9730ca8520525c22ec81c3541d0
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-08-06 21:46:52 -0300

    códigos

---
+++ flask-api/api.py
@@ -0,0 +1,45 @@
+# módulos necessários do flask
+from flask import Flask, request, jsonify
+
+# módulos para crawler das informações
+import requests
+from bs4 import BeautifulSoup
+
+# criar app Flask
+app = Flask(__name__, static_url_path='')
+
+# mapeamento das URLs dos cursos para crawler
+curso_urls = {
+    'IA':'https://www.impacta.edu.br/mba/artificial-intelligence',
+    'Big Data':'https://www.impacta.edu.br/mba/business-intelligence-e-analytics',
+    'DE': 'https://www.impacta.edu.br/mba/data-engineering',
+    'Marketing': 'https://www.impacta.edu.br/mba/executivo-em-marketing-digital',
+    'Full Stack': 'https://www.impacta.edu.br/mba/full-stack-developer',
+    'UX': 'https://www.impacta.edu.br/mba/ux-design-digital-experience',
+    'Cloud': 'https://www.impacta.edu.br/mba/cloud-computing-devops'
+}
+
+
+# endpoint do webhook - POST
+@app.route('/chatbot',  methods=['POST'])
+def watsonwebhook():
+  # retornar dados enviados no formato JSON
+  req = request.get_json()
+
+  # tipo da task enviada: info ou email
+  task = req['task']
+  if task =='info':
+
+    # nome do curso enviado
+    curso = req['curso']
+
+    #crawler para pegar informações
+    r = requests.get(curso_urls[curso]).text
+    info = BeautifulSoup(r, 'html.parser')(class_='background-base-site')[0].text
+
+    #retornar informações no campo info, no formato JSON
+    return jsonify({'info':info})
+    
+# executar app flask
+if __name__ == '__main__':
+  app.run()

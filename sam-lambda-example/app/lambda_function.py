163afc44c81c5080f3b80f868ea00dba51629ee5
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-09-17 13:46:23 -0300

    atualizações gerais

--- sam-lambda-example/app/lambda_function.py
+++ sam-lambda-example/app/lambda_function.py
@@ -1,53 +1,10 @@
-import json
-
-# módulos para crawler das informações
-import requests
-from bs4 import BeautifulSoup
-
-# mapeamento das URLs dos cursos para crawler
-curso_urls = {
-    'IA':'https://www.impacta.edu.br/mba/artificial-intelligence',
-    'Big Data':'https://www.impacta.edu.br/mba/business-intelligence-e-analytics',
-    'DE': 'https://www.impacta.edu.br/mba/data-engineering',
-    'Marketing': 'https://www.impacta.edu.br/mba/executivo-em-marketing-digital',
-    'Full Stack': 'https://www.impacta.edu.br/mba/full-stack-developer',
-    'UX': 'https://www.impacta.edu.br/mba/ux-design-digital-experience',
-    'Cloud': 'https://www.impacta.edu.br/mba/cloud-computing-devops'
-}
 
 def lambda_handler(event, context):
     print("EVENTO: ", event)
-    task = event['task']
-    if task =='info':
-
-        # nome do curso enviado
-        curso = event['curso']
-
-        #crawler para pegar informações
-        nome_curso = curso_urls.get(curso)
-        if nome_curso:
-            r = requests.get(nome_curso).text
-            info = BeautifulSoup(r, 'html.parser')(class_='background-base-site')[0].text
-
-            retorno =  {
-                'statusCode': 200,
-                'body': json.dumps({'info': info})
-            }
-            print(retorno)
-            return retorno
-
-        # curso não encontrado
-        else:
-            retorno =  {
-                'statusCode': 404,
-                'body': json.dumps({'info': 'Não encontramos informações sobre este curso'})
-            }
-            print(retorno)
-            return retorno
-        
+               
     retorno =  {
         'statusCode': 404,
-        'body': json.dumps({'erro': 'Tarefa não encontrado'})
+        'body': event.get('queryStringParameters').get('mensagem') if event.get('queryStringParameters') else event.get('body').get('mensagem') 
     }
     
-    print(retorno)
+    return retorno
---
+++ sam-lambda-info-crawler/README.md
@@ -0,0 +1,43 @@
+# cloudbots-codes
+
+## Implantar função lambda com SAM
+
+### Configurar variáveis de ambiente para conectar na AWS
+
+Windows/PowerShell
+
+```powershell
+$Env:AWS_DEFAULT_REGION="regiao"
+$Env:AWS_ACCESS_KEY_ID="aws_access_key_id"
+$Env:AWS_SECRET_ACCESS_KEY="aws_secret_access_key"
+$Env:AWS_SESSION_TOKEN="aws_session_token"
+```
+
+Linux
+
+```bash
+export AWS_DEFAULT_REGION=regiao
+export AWS_ACCESS_KEY_ID=aws_access_key_id
+export AWS_SECRET_ACCESS_KEY=aws_secret_access_key
+export AWS_SESSION_TOKEN=aws_session_token
+```
+
+### Implantar com SAM
+
+Construir código
+
+```bash
+sam build
+```
+
+Criar pacote
+
+```bash
+sam package --region us-east-1 --resolve-s3
+```
+
+Implantar
+
+```bash
+sam deploy --stack-name amazon-lex-bob-stack --region us-east-1 --capabilities CAPABILITY_IAM --resolve-s3 --parameter-overrides role=FuncaoIAMPermissao
+```
